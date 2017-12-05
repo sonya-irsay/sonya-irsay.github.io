72bc446a54709f03a53a6968427d474866b5f9c5
sonya-irsay <sanie@newschool.edu>
2017-12-05 01:26:58 +0100

    upload one page website

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // $("#demo").html("Hello, World!");
+});
+
+// $('#hold').hover(
+//   function() {
+//     $('#workimg').fadeIn('fast');
+//   },
+//   function() {
+//     $('#workimg').fadeOut('fast');
+//   }
+// );
