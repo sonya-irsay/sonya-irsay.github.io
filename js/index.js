e1fb09ecfbbf6bdd8666f9dacdee32d73d6e09b8
sanie-i <sanie96@gmail.com>
2018-12-11 20:18:46 +0100

    add text toggle in feed

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ var height = window.innerHeight;
 var wire;
 
 var title = "Title — Suisse Regular";
+var titleFeed = "Title — Suisse Light";
 var details = "Details — Suisse Light Italic";
 var description = "Description — Suisse Light";
 var disciplines = "Disciplines — Suisse Light Italic"
@@ -124,7 +125,15 @@ function displayFontSize() {
     $('#laysan-dis').html(disciplines + " " + sInsert + "px");
 
     //Feed
-    // $('#feed-1').html(title + " " + sInsert +"px");
+    $('#feed-1').html(titleFeed + " " + sInsert +"px");
+    $('#feed-2').html(titleFeed + " " + sInsert +"px");
+    $('#feed-3').html(titleFeed + " " + sInsert +"px");
+    $('#feed-4').html(titleFeed + " " + sInsert +"px");
+    $('#feed-5').html(titleFeed + " " + sInsert +"px");
+    $('#feed-6').html(titleFeed + " " + sInsert +"px");
+    $('#feed-7').html(titleFeed + " " + sInsert +"px");
+    $('#feed-8').html(titleFeed + " " + sInsert +"px");
+    $('#feed-9').html(titleFeed + " " + sInsert +"px");
   }
 };
 
@@ -150,36 +159,44 @@ function toggleWireframe() {
 
   //TEXT CHAHNGES
   if (wire) {
-      //button text
-      buttonContent();
-      // $('.wireframe-button').html('Content');
-      displayFontSize();
-      //text changes
-      window.onresize = displayFontSize;
-    } else {
-      //button text
-      buttonWire();
-      // $('.wireframe-button').html('Wireframe');
+    //button text
+    buttonContent();
+    // $('.wireframe-button').html('Content');
+    displayFontSize();
+    //text changes
+    window.onresize = displayFontSize;
+  } else {
+    //button text
+    buttonWire();
+    // $('.wireframe-button').html('Wireframe');
 
-      //text changes
-      //Mistaek Magazine
-      $('#mistaek').html('Mistaek Magazine');
-      $('#mistaek-2').html('Mistaek Magazine');
-      $('#mistaek-det').html('Issue I, perfect bound, 160 pages.');
-      $('#mistaek-des-1').html('Mistaek Magazine is about mistakes. Mistaek Magazine makes mistakes. The magazine’s conception coincided with, or rather came out of, the grave and radical changes rapidly unfolding on a global scale in the period around November 2016.');
-      $('#mistaek-des-2').html('Mistaek Magazine tries to oppose the tendency to regress into cynicism or nihilism, instead proposing the possibility of cultivating self-conscious, critical reflection. Mistaek Mag seeks to examine humankind’s immense database of everything that has gone wrong, from silly typos to severe global disasters to the faults inherent in the design field itself, aiming to look critically at itself, at ourselves.');
-      $('#mistaek-dis').html('Art Direction, Editorial Design, Print');
+    //text changes
+    //Mistaek Magazine
+    $('#mistaek').html('Mistaek Magazine');
+    $('#mistaek-2').html('Mistaek Magazine');
+    $('#mistaek-det').html('Issue I, perfect bound, 160 pages.');
+    $('#mistaek-des-1').html('Mistaek Magazine is about mistakes. Mistaek Magazine makes mistakes. The magazine’s conception coincided with, or rather came out of, the grave and radical changes rapidly unfolding on a global scale in the period around November 2016.');
+    $('#mistaek-des-2').html('Mistaek Magazine tries to oppose the tendency to regress into cynicism or nihilism, instead proposing the possibility of cultivating self-conscious, critical reflection. Mistaek Mag seeks to examine humankind’s immense database of everything that has gone wrong, from silly typos to severe global disasters to the faults inherent in the design field itself, aiming to look critically at itself, at ourselves.');
+    $('#mistaek-dis').html('Art Direction, Editorial Design, Print');
 
-      //Laysan
-      $('#laysan').html('Laysan View');
-      $('#laysan-det').html('Creative direction: Print Plus Sign');
-      $('#laysan-des-1').html('Logo and visual identity for Toronto contracting firm. The firm’s name (Laysan: an albatross, a sea bird) serves as the starting point for a playful and versatile identity made up of modular grid elements.');
-      $('#laysan-des-2').html('The grid system of the logo expands into patterns, illustrations and pictograms representing the firm’s services.');
-      $('#laysan-des-3').html('The base grid is revealed in letterheads and contract forms.');
-      $('#laysan-dis').html('Identity, Logo, Print');
+    //Laysan
+    $('#laysan').html('Laysan View');
+    $('#laysan-det').html('Creative direction: Print Plus Sign');
+    $('#laysan-des-1').html('Logo and visual identity for Toronto contracting firm. The firm’s name (Laysan: an albatross, a sea bird) serves as the starting point for a playful and versatile identity made up of modular grid elements.');
+    $('#laysan-des-2').html('The grid system of the logo expands into patterns, illustrations and pictograms representing the firm’s services.');
+    $('#laysan-des-3').html('The base grid is revealed in letterheads and contract forms.');
+    $('#laysan-dis').html('Identity, Logo, Print');
 
-      //Feed
-      // $('#feed-1').html('Silkscreen poster, 33 x 45 cm, edition of 20, 7 colors, on white, gray and cream Arches paper.');
+    //Feed
+    $('#feed-1').html('Poster for Merge, featured visual made with C++ in Openframeworks for live VJ performance. Platforme Gallery, Paris.');
+    $('#feed-2').html('Poster for Merge, featured visual by Kris Madden, made with C++ in Openframeworks for live VJ performance. Platforme Gallery, Paris.');
+    $('#feed-3').html('Typographic poster for a festival. Silkscreen, 22 x 30, 4-color, CMYK color halftone. Communication Arts Typography Annual Award.');
+    $('#feed-4').html('Set of postcards, riso print (2 colors) on Munken Print 300g paper, edition of 30. Collaborative project led by Aurelien Farina of Paper Tiger.');
+    $('#feed-5').html('A book compiling research and explorations in pattern.');
+    $('#feed-6').html('In progress — auction catalogue layouts for charity auction in support of world heritage sites in danger. Clients: UNESCO, Sotheby\'s and L\'ecole du management de la culture et du marche de l\'art, Paris.');
+    $('#feed-7').html('Silkscreen poster, 33 x 45 cm, edition of 20, 7 colors, on white, gray and cream Arches paper.');
+    $('#feed-8').html('Preliminary sketch for "of the river, of the forest.');
+    $('#feed-9').html('3D meshes reacting to sound, coded in C++ with OpenFrameworks.');
   }
 
   console.log("wire toggled");
