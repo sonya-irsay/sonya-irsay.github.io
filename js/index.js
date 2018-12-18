d42fac878030236a924afbc2a4207b6bb913cbbe
sanie-i <sanie96@gmail.com>
2018-12-18 23:00:33 +0100

    lazy load try

--- js/index.js
+++ js/index.js
@@ -18,6 +18,29 @@ var smoothY = 0;
 // showing class: show
 // the button: infoButton
 // the existing class: contact-box
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Possibly fall back to a more compatible method here
+  }
+});
 
 function toggleInfo() {
   $("#info").toggleClass("show");
